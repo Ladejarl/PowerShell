33c269329a31eb12c801f57f8a6e5ffc3c1a1f75
Ladejarl <46592928+Ladejarl@users.noreply.github.com>
2019-01-11 10:05:43 +0100

    Add files via upload

---
+++ Collect name and email from AD group PowerShell.ps1
@@ -0,0 +1,8 @@
+﻿#This script collects name and email from the Active Directory  group defined, exports to a CSV file as you define with a path and filename.
+#Run directly on your AD server. If it does not work, server might be running an older version of PowerShell.
+#Define Groupname as needed, but keep the quotes.
+
+Get-ADGroupMember -Identity "Groupname" -Recursive | 
+Get-ADUser -Properties Name,Mail | 
+Select-Object Name,Mail | 
+Export-CSV -Path C:\Scripts\Groupname.csv -NoTypeInformation
